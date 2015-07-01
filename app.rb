7ce9dfca8060721679023b573026d8412c9abd4f
AnnaErshova <anna.ershova@gmail.com>
2015-07-01 16:00:49 -0400

    done-ish

--- app.rb
+++ app.rb
@@ -2,4 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :"/super_hero"
+  end
+
+  get '/teams' do
+    erb :"/team"
+  end
+
+  post '/teams' do
+    @team = params[:team]
+  end
+
 end
